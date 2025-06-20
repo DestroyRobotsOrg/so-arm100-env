089e24d7f303405dc5e779e27d65bb55564841e8
Carlos Diaz-Padron <carlosdiazpadron@gmail.com>
2025-06-19 22:39:36 -0700

    fix

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import gymnasium as gym
 from gantry import Environment, render_sphere
 import example
 
-render_sphere([0, 0, 0], 0.2)
-# gym_env = gym.make("so-arm100")
-# env = Environment(gym_env)
-# env.render()
+gym_env = gym.make("so-arm100")
+env = Environment(gym_env)
+env.render()
 
