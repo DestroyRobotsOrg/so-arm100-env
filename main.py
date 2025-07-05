8b1b684048e4c87097f929a75de697c1710b9fa9
Carlos Diaz-Padron <carlosdiazpadron@gmail.com>
2025-07-05 00:20:34 -0700

    use animation

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from gantry import Environment
 import example
 
 gym_env = gym.make("so-arm100")
-env = Environment(gym_env)
-env.reset(seed=42)
-env.render()
+env = Environment(gym_env, fps=10)
+observation, info = env.reset(seed=42)
+
+with env.render_settings(fps=10):
+  for _ in range(2000):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+    env.render()
+
+    if terminated or truncated:
+      observation, info = env.reset()
 
